732e55ece562d2d24065c3c0629fde140427ab85
Kushal Vardhan <vardhank140102@gmail.com>
2024-11-30 20:42:00 +0530

    footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,10 @@
-import sign from "./assets/sign.png"
+import sign from "./assets/sign.png";
 function App() {
   return (
     <>
-    <h1>kushal vardhan</h1>
-   <img src={sign} alt="" />
+      <h1>kushal vardhan</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

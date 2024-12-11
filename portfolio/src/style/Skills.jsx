fd5a9b567a168c4de61fdd262915d96b217ba0a7
Kushal Vardhan <vardhank140102@gmail.com>
2024-12-11 20:37:30 +0530

    skills

--- portfolio/src/style/Skills.jsx
+++ portfolio/src/style/Skills.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Skills() {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        <li>HTML And CSS</li>
+        <li>JavaScript</li>
+        <li>Java</li>
+        <li>DSA</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Skills;

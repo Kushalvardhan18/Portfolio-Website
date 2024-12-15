3d7159e27eaf679faf9f16dacc49d74bbf672347
Kushal Vardhan <vardhank140102@gmail.com>
2024-12-15 21:14:17 +0530

    footer exported

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,10 @@
-import Header from "./components/Header";
-
+import { Header } from "./components";
+import { Footer } from "./components";
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }

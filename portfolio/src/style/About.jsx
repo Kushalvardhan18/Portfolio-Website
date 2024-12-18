b9e2ffe30ea2a16d099436a1492283056df2f7ae
Kushal Vardhan <vardhank140102@gmail.com>
2024-12-18 20:35:11 +0530

    about

--- portfolio/src/style/About.jsx
+++ portfolio/src/style/About.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Hi, I'm Kushal Vardhan, a 24-year-old{" "}
+        <span style="color: #c778dd;">Software Developer</span> with a passion
+        for problem-solving and exploring the intricacies of technology. I
+        specialize in Java programming, with expertise in{" "}
+        <span style="color: #c778dd;">Algorithms</span>,{" "}
+        <span style="color: #c778dd;">Data Structures</span>,{" "}
+        <span style="color: #c778dd;">JAVA </span>and
+        <span style="color: #c778dd;">Web Development </span>.
+      </p>
+      <p>
+        With a strong foundation in software development and a track record of
+        achievements like a <span style="color:#c778dd;">100-day</span> streak
+        on LeetCode, I thrive on tackling challenges and continuously expanding
+        my skill set.
+      </p>
+      <p>
+        In my free time, you might find me experimenting with projects or
+        enhancing my GitHub portfolio. Let's connect and create something
+        impactful together!
+      </p>
+    </div>
+  );
 }
 
 export default About;
